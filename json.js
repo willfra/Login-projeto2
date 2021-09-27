e87a313339105c1a05c7f77083e44235a534e44d
William França <61748715+willfra@users.noreply.github.com>
2021-09-27 18:53:48 -0300

    atualização caminhos

--- json.js
+++ json.js
@@ -6,3 +6,10 @@ function play(){
 function pause(){
     stream.pause();
 }
+
+
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function () {
+    document.querySelector(".menu-lateral").classList.toggle("close-menu");
+});
